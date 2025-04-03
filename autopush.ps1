774c3dfa3d2fbb07fc465291782f8e524ff0418f
Evhan Linget <evhan.linget.pro@gmail.com>
2025-04-03 04:23:28 +0200

    ðŸš€ Auto-commit : mise Ã  jour en local âœ¨ | ðŸ•’ 2025-04-03 04:23:28 | ðŸ„ Synchro automatique depuis l'environnement de dÃ©veloppement

--- autopush.ps1
+++ autopush.ps1
@@ -1,5 +1,5 @@
-#!/bin/bash
-
+$time = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+$message = "🚀 Auto-commit : mise à jour en local ✨ | 🕒 $time | 🔄 Synchro automatique depuis l'environnement de développement"
 git add .
-git commit -m "auto-commit $(date '+%Y-%m-%d %H:%M:%S')"
+git commit -m "$message"
 git push origin main
