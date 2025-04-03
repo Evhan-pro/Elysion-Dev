fe9d741c1270a0c9432595c8a4f0fc620d50fc07
Evhan Linget <evhan.linget.pro@gmail.com>
2025-04-03 05:16:30 +0200

    [auto-sync] 🛠️ Code update from dev @ 03/04/2025 05:16:28 | Triggered by local save (CTRL+S)

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
@@ -9,6 +9,4 @@ const userSchema = mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
-
-export default User;
+export default mongoose.model("User", userSchema);
